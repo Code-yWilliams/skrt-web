4c529140046f389dca4e86a5d1ee1e3a5ee09055
Cody Williams <cody.williams@untappd.com>
2024-07-14 20:41:29 -0400

    api login and logout controllers

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,47 @@
+module Api
+  module V1
+    class SessionsController < ApiController
+      skip_before_action :authenticate_user!, only: [:create]
+
+      # POST /api/v1/sessions
+      def create
+        user = User.find_by!(email: params[:email]&.downcase)
+        validate_password!(user, params[:password])
+
+        user.track_last_seen!
+
+        # only allowed users can get access to the API
+        authorize user, :create_auth_tokens?
+
+        user_session = get_user_session!(user)
+
+        render(json: user_session)
+      rescue ActiveRecord::RecordNotFound
+        raise Api::V1::Errors::LoginFailedError
+      end
+
+      # DELETE /api/v1/sessions
+      def destroy
+        user = @current_user
+        authorize user, :destroy_auth_tokens?
+        user.update!(
+          mobile_auth_token: nil
+        )
+        render(json: {status: "OK"}, status: :ok)
+      end
+
+      def validate_password!(user, password)
+        raise Api::V1::Errors::LoginFailedError unless user.valid_password?(password)
+      end
+
+      def get_user_session!(user)
+        {
+          user: {
+            email: user.email,
+            mobile_auth_token: user.mobile_auth_token,
+          }
+        }
+      end
+    end
+  end
+end
