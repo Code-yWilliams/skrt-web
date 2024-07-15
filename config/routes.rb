de85321c4c5c2472eab960d87ecca7906503a543
Cody Williams <cody.williams@untappd.com>
2024-07-14 20:38:52 -0400

    configure core application routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,29 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  def draw(routes_name)
+    path_parts = %w[config routes] + routes_name.to_s.split(File::Separator)
+    file_name = "#{path_parts.pop}.rb"
+    instance_eval(File.read(Rails.root.join(*path_parts, file_name)))
+  end
+  
+  # User login, logout, and forgot password routes
+  devise_for :users, path: "", path_names: {
+    sign_up: "signup",
+    sign_in: "login",
+    sign_out: "logout",
+    password: "forgot-password"
+  }, controllers: {
+    sessions: "users/sessions",
+    registrations: "users/registrations",
+    passwords: "users/passwords",
+    confirmations: "users/confirmations",
+    unlocks: "users/unlocks"
+  }
+  
+  unauthenticated :user do
+    get "/" => redirect("/login")
+  end
+
+  draw :api
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
