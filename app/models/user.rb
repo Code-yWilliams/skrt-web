91761a3165aae751c258d44071422f33235e3f1d
Cody Williams <cody@codywilliams.dev>
2024-08-14 18:32:52 -0700

    remove migrations with duplicate schema changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,7 @@
 #  current_sign_in_ip     :string
 #  email                  :string           default(""), not null
 #  encrypted_password     :string           default(""), not null
+#  failed_attempts        :integer          default(0), not null
 #  last_seen              :datetime
 #  last_sign_in_at        :datetime
 #  last_sign_in_ip        :string
@@ -18,15 +19,18 @@
 #  remember_created_at    :datetime
 #  reset_password_sent_at :datetime
 #  reset_password_token   :string
-#  sign_in_count          :integer
+#  sign_in_count          :integer          default(0), not null
 #  unconfirmed_email      :string
+#  unlock_token           :string
 #  created_at             :datetime         not null
 #  updated_at             :datetime         not null
 #
 # Indexes
 #
+#  index_users_on_confirmation_token    (confirmation_token) UNIQUE
 #  index_users_on_email                 (email) UNIQUE
 #  index_users_on_reset_password_token  (reset_password_token) UNIQUE
+#  index_users_on_unlock_token          (unlock_token) UNIQUE
 #
 class User < ApplicationRecord
   include UserAuthTokenOverrides
@@ -38,4 +42,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :confirmable, :lockable, :trackable, :lastseenable
+
+
+  private
+
+  def reset_mobile_auth_token
+    self.mobile_auth_token = nil
+  end
 end
--- db/migrate/20240706023751_add_current_sign_in_at_at_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddCurrentSignInAtAtToUsers < ActiveRecord::Migration[7.1]
-  def change
-    add_column :users, :current_sign_in_at, :datetime
-  end
-end
