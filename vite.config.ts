745ca82afaea0f4ea2cf349b129d487c7e27cbe0
Cody Williams <cody@codywilliams.dev>
2024-09-16 09:14:30 -0700

    install tanstack router

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from "vite";
 import RubyPlugin from "vite-plugin-ruby";
 import react from "@vitejs/plugin-react";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [RubyPlugin(), react()],
+  plugins: [RubyPlugin(), tsConfigPaths(), TanStackRouterVite(), react()],
 });
